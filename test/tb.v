eeef027069b207d05826e5df9d2f914618e62d75
Neelesh-04 <139808666+Neelesh-04@users.noreply.github.com>
2025-02-23 11:17:45 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,4 +1,4 @@
-`default_nettype none
+// `default_nettype none
 `timescale 1ns / 1ps
 
 /* This testbench just instantiates the module and makes some convenient wires
@@ -7,43 +7,65 @@
 module tb ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
-  initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
-    #1;
-  end
+  // initial begin
+  //   $dumpfile("tb.vcd");
+  //   $dumpvars(0, tb);
+  //   #1;
+  // end
 
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
+  reg mode;
+reg read_en;
+   reg write_en;
+   wire [7:0] uo_out;
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_top user_project (
 
       // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
+      
+     .ui_in  ({mode,read_en,write_en,5'b00000}),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
+     .uio_in (8'b0),   // IOs: Input path
+      .uio_out(),  // IOs: Output path
+      .uio_oe (),   // IOs: Enable path (active high: 0=input, 1=output)
+     .ena    (1'b1),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
+initial clk = 0;
+always #50 clk = ~clk; 
 
+initial begin
+rst_n = 1;
+#120
+rst_n =0;
+mode = 1;
+write_en = 1;
+read_en = 0;
+#3000
+write_en = 0;
+read_en = 1;
+#3000
+mode = 0;
+write_en = 1;
+read_en = 0;
+#3000
+write_en = 0;
+read_en = 1;
+#3000
+write_en = 0;
+read_en = 0;
+
+#50000 $finish;
+end
+
+   initial begin
+        
+        $dumpfile("fifo.vcd");
+        $dumpvars;
+    end
 endmodule
+
